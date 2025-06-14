e960f39548f00dd55df095e145c365d731c8c204
Samuel Theophilus <samueltheophilus019@gmail.com>
2025-06-14 07:07:47 +0100

    feat(listings): add initial property listings data in JSON format

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { JsonLogger } from './logger/logger.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: new JsonLogger(),
+  });
+  
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
+    maxAge: 3600,
+  });
+  
+  app.use(cookieParser());
+  
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  
+  const config = new DocumentBuilder()
+  .setTitle('Property Listings API')
+  .setDescription('API for listing, filtering, and managing properties')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
